9fb714e0edec6cde6d8e6ca1c1ff0adb299e79ee
kubersharma001 <kubersharma001@gmail.com>
2021-08-17 17:37:41 +0000

    sm6150-common: Compile libldacBT_bco.so
    08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I910382a3affe8d02d6b0b20fd2dc3e91e57f3e9a

--- sm6150.mk
+++ sm6150.mk
@@ -81,6 +81,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
